562e540bb14415df6b8d588c07b15c8554a81ddb
ktulhoo86 <ktulhoo.pyneng@yandex.ru>
2021-10-19 23:47:45 +0300

    Flask-Migrations и регистрация пользователей

--- webapp/user/views.py
+++ webapp/user/views.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template, flash, redirect, url_for
 from flask_login import current_user, login_user, logout_user
 
-from webapp.user.forms import LoginForm
+from webapp.db import db
+from webapp.user.forms import LoginForm, RegistrationForm
 from webapp.user.models import User
 
 blueprint = Blueprint('user', __name__, url_prefix='/users')
@@ -28,7 +29,7 @@ def procces_login():
             flash('Вы успешно вошли на сайт')
             return redirect(url_for('news.index'))
     flash('Не правильно е имя пользователя или пароль')
-    return redirect(url_for('login'))
+    return redirect(url_for('users.login'))
 
 
 @blueprint.route('/logout')
@@ -36,3 +37,28 @@ def logout():
     logout_user()
     flash('Вы успешно разлогинились')
     return redirect(url_for('news.index'))
+
+
+@blueprint.route('/register')
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('news.index'))
+    title = 'Регистрация'
+    form = RegistrationForm()
+    return render_template('user/registration.html',
+                           page_title=title, form=form)
+
+
+@blueprint.route('/process-reg', methods=['POST'])
+def process_reg():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        news_user = User(username=form.username.data,
+                         email=form.email.data, role='user')
+        news_user.set_password(form.password.data)
+        db.session.add(news_user)
+        db.session.commit()
+        flash('Вы успешно зарегистрировались!')
+        return redirect(url_for('user.login'))
+    flash('Форма заполнена не верно!')
+    return redirect(url_for('user.register'))
